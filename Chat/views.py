3590ef0e44dc3d5997c2dfc4a2e1283e6d47d1e1
hansol <hanla01@naver.com>
2016-02-08 19:32:24 +0900

    로그인 기능과 채팅방 선택, DB에 저장하는 형태의 채팅방 구현

--- Chat/views.py
+++ Chat/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Chat.models import User, ChatRooms, Message
 import templates
 
 def main(request):
 	return render(request, 'main.html')
+
+def login_check(request):
+    if 'username' in request.POST:
+        if len(request.POST['username']) == 0:
+            return HttpResponse('닉네임을 입력하세요')
+        else:
+            input_name = request.POST['username']
+    else:
+        return HttpResponse('닉네임을 입력하세요2')
+
+    try:
+        user = User.objects.get(Username=input_name)
+        request.session['username'] = input_name
+        return redirect('/chat/')
+    except:
+        return HttpResponse('닉네임이 존재하지 않습니다')
 
 def register(request):
     return render(request, 'register.html')
@@ -38,3 +54,44 @@ def register_check(request):
         except:
             return HttpResponse('아이디 저장 오류')
         return HttpResponse('알 수 없는 에러')
+
+def chat_view(request):
+    username = request.session['username']
+    chattingRoom = ChatRooms.objects.all()
+    
+    return render(request, 'chat_view.html', {
+            "username": username,
+            "ChatRooms": chattingRoom,
+    })
+
+def room_view(request, room_id=None):
+    username = request.session['username']
+    messages = Message.objects.filter(ChatRooms=room_id)
+    return render(request, 'room_view.html', {
+            "room_id": room_id,
+            "username": username,
+            "messages": messages,
+    })
+
+def send(request):
+    if 'message' in request.POST:
+        if len(request.POST['message']) == 0:
+            return HttpResponse('메세지를 입력하세요')
+        else:
+            content = request.POST['message']
+    else:
+        return HttpResponse('메세지를 입력하세요')
+
+    username = request.session['username']
+    user = User.objects.get(Username=username)
+
+    room_id = request.POST['room_id']
+    room = ChatRooms.objects.get(id=room_id)
+
+    try:
+        new_message = Message(Username=user,ChatRooms=room,Content=content)
+        new_message.save()
+        return redirect('/rooms/'+room_id+'/')
+    except:
+        return HttpResponse('메세지 저장 에러')
+
--- ChatRooms/urls.py
+++ ChatRooms/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
     url(r'^register/', 'Chat.views.register'),
     url(r'^register_check/', 'Chat.views.register_check'),
     url(r'^login_check/', 'Chat.views.login_check'),
+    url(r'^chat/', 'Chat.views.chat_view'),
+    url(r'^rooms/(?P<room_id>[0-9]+)/$', 'Chat.views.room_view'),
+    url(r'^send/', 'Chat.views.send'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
